3b4082f0355438f5b5c0d1d4c01a186be347b99a
Luisoooooooooo <luisalfonsovazquezsanchez@gmail.com>
2025-02-03 19:10:04 +0100

    Refactor: Improve efficiency by reusing time data

--- launcher.js
+++ launcher.js
@@ -1,27 +1,21 @@
+const formatTime = (value) => value.toString().padStart(2, '0');
+
 function clock() {
     const timer = new Date();
-    const hours = timer.getHours();
-    const minutes = timer.getMinutes();
-    const seconds = timer.getSeconds();
+    const hours = formatTime(timer.getHours());
+    const minutes = formatTime(timer.getMinutes());
+    const seconds = formatTime(timer.getSeconds());
 
-    console.log(`${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`)
+    console.log(`${hours}:${minutes}:${seconds}`);
 }
-/* setInterval(clock, 1000); */
+
 function greeting() {
     const timer = new Date();
-    const hours = timer.getHours();
-    const minutes = timer.getMinutes();
-    const seconds = timer.getSeconds();
+    const hours = formatTime(timer.getHours());
 
-    let greeting = "";
-
-    if(hours >= 6 && hours < 12) {
-        greeting = "Good morning!";
-    } else if(hours >= 12 && hours < 18) {
-        greeting = "Good evening!";
-    } else {
-        greeting = "Good night!";
-    }
+    const greeting = (hours >= 6 && hours < 12) ? "Good morning!"
+    : (hours >= 12 && hours < 18) ? "Good evening!"
+    : "Good night!";
 
     console.log(greeting);
 }
